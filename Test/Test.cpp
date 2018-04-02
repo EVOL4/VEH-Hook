90b446a45cd28434a04a9301c1bccd94cd7281ee
EVOL4 <meryddian@gmail.com>
2018-04-02 16:14:49 +0800

    synchronized some threads

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -9,6 +9,7 @@ int main()
 	MessageBox(NULL, L"the fact infor222", L"test SEH hook", MB_OK);
 
 	HMODULE lib = LoadLibrary(L"..\\Debug\\VehHookDll.dll");	//小Demo,不去考虑注入时修正函数地址之类的问题
+	Sleep(2000);
 
 	MessageBox(NULL, L"the fact infor222", L"test SEH hook", MB_OK);
 }
